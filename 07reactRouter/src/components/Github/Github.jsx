7be7243c6c59f1a94e094feedc303f5ace2a7886
sagar <myemail@email.com>
2024-03-31 22:09:12 +0545

    refreshed github page

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
 
 function Github() {
-    return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
-        Github followers: 
-        </div>
-    )
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/hiteshchoudhary")
+      .then((response) => response.json())
+      .then((convertedJSON) => {
+        console.log(convertedJSON);
+        setData(convertedJSON);
+      });
+  }, []);
+
+  const followers = data.followers
+
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      <span className="image">
+        <img src={data.avatar_url} alt="Git picture" width={300} />
+      </span>
+
+      
+      <span className="Info">
+      <p>Name: {data.name},</p>
+      <p>Username: {data.login}</p>
+      Github followers: {followers}
+      </span>
+      
+    </div>
+  );
 }
 
 export default Github;
