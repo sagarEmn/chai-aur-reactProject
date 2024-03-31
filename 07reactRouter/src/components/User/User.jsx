6698ca562ff4f23f76fc5014a9ce0d2e4bf38ac5
sagar <myemail@email.com>
2024-03-31 16:55:19 +0545

    added github page

--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useParams } from 'react-router-dom';
 
 function User() {
+  const { userid } = useParams() 
   return (
-    <>
-    User: 
-    </>
+    <div className="text-3xl bg-gray-700 text-white p-28">
+    User: {userid}
+    </div>
   )
 }
 
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -7,7 +7,8 @@ import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
-import User from "./components/Contact/Contact.jsx";
+import User from "./components/User/User.jsx";
+import Github from "./components/Github/Github.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -15,7 +16,8 @@ const router = createBrowserRouter(
       <Route path="" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
-      <Route path="user/:id" element={<User />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route path="github" element={<Github />} />
     </Route>
   )
 );
