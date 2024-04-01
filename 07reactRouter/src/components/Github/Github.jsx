081699cf8e2d6192407b8ccd9c4ff391054fcf0a
sagar <myemail@email.com>
2024-04-01 22:10:55 +0545

    modified github fetch

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 function Github() {
   const [data, setData] = useState([]);
   useEffect(() => {
-    fetch("https://api.github.com/users/hiteshchoudhary")
+    fetch("https://api.github.com/users/sagarEmn")
       .then((response) => response.json())
       .then((convertedJSON) => {
         console.log(convertedJSON);
@@ -12,23 +12,28 @@ function Github() {
       });
   }, []);
 
-  const followers = data.followers
+  const followers = data.followers;
 
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
-      <span className="image">
-        <img src={data.avatar_url} alt="Git picture" width={300} />
-      </span>
+      <div className="flex flex-1">
+        <span className="image">
+          <img src={data.avatar_url} alt="Git picture" width={300} />
+        </span>
 
-      
-      <span className="Info">
-      <p>Name: {data.name},</p>
-      <p>Username: {data.login}</p>
-      Github followers: {followers}
-      </span>
-      
+        <span className="Info flex flex-col justify-center items-center">
+          <p>Name: {data.name},</p>
+          <p>Username: {data.login}</p>
+          Github followers: {followers}
+        </span>
+      </div>
     </div>
   );
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/hiteshchoudary");
+  return response.json();
+};
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -17,7 +17,10 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
       <Route path="user/:userid" element={<User />} />
-      <Route path="github" element={<Github />} />
+      <Route 
+      loader={}
+      path="github" 
+      lement={<Github />} />
     </Route>
   )
 );
